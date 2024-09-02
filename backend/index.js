32bc7037f1db18c48dcd06baeacb8601a299d42d
ugyen.dev <dev.ugyen@gmail.com>
2024-09-02 20:53:20 +0600

    create signup api route

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
 
+//middlewares
+app.use(express.json());
+
 const port = process.env.PORT || 8000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+//database connection
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI).then(() => {
@@ -20,7 +25,9 @@ const connectDB = async () => {
   }
 };
 
+//routes
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
