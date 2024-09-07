54d76d0f7aea031da824a8e449075921dd8649ac
ugyen.dev <dev.ugyen@gmail.com>
2024-09-08 05:55:26 +0600

    create redux store and manage state

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -15,8 +22,7 @@ export default function Signin() {
     e.preventDefault();
 
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
 
       const response = await fetch("/backend/api/auth/signin", {
         method: "POST",
@@ -26,23 +32,22 @@ export default function Signin() {
         body: JSON.stringify(formData),
       });
       const data = await response.json();
-      setLoading(false);
 
       if (data.success == false) {
-        setError(true);
+        dispatch(signInFailure(data.message || "Failed to sign in"));
         return;
       }
-      window.location.href = "/";
+
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(true);
-      console.log(error);
+      dispatch(signInFailure(error || "Failed to sign in"));
     }
   };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Signin</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -74,7 +79,9 @@ export default function Signin() {
           </Link>
         </p>
       </div>
-      <p className="text-red-500 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-red-500 mt-5">
+        {error ? error || "Something went wrong!" : " "}
+      </p>
     </div>
   );
 }
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.currentUser = null;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure, logout } =
+  userSlice.actions;
+export default userSlice.reducer;
